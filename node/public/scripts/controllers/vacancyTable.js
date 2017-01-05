a140cd6be6ffce686d019c9501cc6e614e13356f
a.kovalenko <a.kovalenko@ukr.net>
2017-01-05 22:36:28 +0200

    update in ng-init

--- node/public/scripts/controllers/vacancyTable.js
+++ node/public/scripts/controllers/vacancyTable.js
@@ -1,18 +1,5 @@
 "use strict"
-vacancyTable.controller("getAllTable", function($scope, $http){
-    // $scope.nameChange = function(){
-    //  $scope.greeting = "Hello " + $scope.name + '!';
-    // }
-    // $scope.nameChange()
 
-    $scope.update = function() {
-        $http.get("/getAll")
-            .success(function(rows) {
-                $scope.rows = rows;
-            });
-    };
-    $scope.update();
-});
 vacancyTable.controller("tablesolo", function($scope, $http){
 	$scope.update = function() {
         $http.get("/getAll")
@@ -20,6 +7,6 @@ vacancyTable.controller("tablesolo", function($scope, $http){
                 $scope.rows = rows;
             });
     };
-    $scope.update();
+    
 });
 
